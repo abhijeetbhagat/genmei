019bdb85b56d33f74dfea2e94c1898dd102b668c
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2017-11-17 22:50:29 +0530

    Make message variants structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,20 @@
+#![feature(use_extern_macros)]
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+#[macro_use]
+extern crate serde_json;
 mod message;
 mod protocol;
+
+
 #[cfg(test)]
 mod tests {
+    use serde_json;
+    use message;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_message_serialization_to_json() {
+        assert_eq!(serde_json::to_string(&message::Message::StreamItem {_type:0, invocationId:String::from("a"), item:serde_json::json!(1)}).unwrap(), 
+                   "{\"StreamItem\":{\"_type\":0,\"invocationId\":\"a\",\"item\":1}}");
     }
 }
--- src/message.rs
+++ src/message.rs
@@ -1,9 +1,13 @@
 use protocol::Protocol;
-enum Message {
-    Negotiation (Protocol),
-    Invocation,
-    StreamInvocation,
-    StreamItem,
-    Completion,
-    CancelInvocation
+use serde_json::Value;
+
+#[derive(Serialize, Deserialize)]
+pub enum Message {
+    Negotiation ,
+    Invocation { _type : i32, invocationId : String, nonblocking : bool, target : String, arguments : Vec<Value> },
+    StreamInvocation { _type: i32, invocationId : String, target : String, arguments : Vec<Value> },
+    StreamItem { _type : i32, invocationId : String, item : Value },
+    Completion { _type : i32, invocationId : String, result : Value, error : String },
+    CancelInvocation { _type : i32, invocationId : String },
+    Ping
 }
