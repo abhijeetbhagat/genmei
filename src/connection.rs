3cb3ec0b525f194b895692db4b124bbad7ae5810
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2017-11-19 19:58:43 +0530

    Use function pointer instead of Fn to simplify things

--- src/connection.rs
+++ src/connection.rs
@@ -1,23 +1,36 @@
 use hubproxy::{HubProxy, Proxy};
 use hubresult::HubResult;
+use std::collections::HashMap;
 
 trait HubConnect {
-    fn register_callback (Fn(HubResult));
-    fn remove_callback (id : String);
+    fn register_callback (&mut self, fn(HubResult));
+    fn remove_callback (&mut self, id : String);
 }
 
 struct HubConnection {
-    url : String
+    url : String,
+    callbacks_map : HashMap<String, fn(HubResult)>
 }
 
 impl HubConnection {
     fn new (url : String) -> Self {
         HubConnection {
-            url : url
+            url : url,
+            callbacks_map : HashMap::new()
         }
     }
 
     fn create_hub_proxy () -> Box<HubProxy> {
         Box::new (Proxy::new ())
+    }
+}
+
+impl HubConnect for HubConnection {
+    fn register_callback (&mut self, cb : fn(HubResult)) {
+        self.callbacks_map.insert (String::from("1"), cb);
+    }
+
+    fn remove_callback (&mut self, id : String) {
+        self.callbacks_map.remove (&id);
     }
 }
--- src/hubproxy.rs
+++ src/hubproxy.rs
@@ -10,10 +10,17 @@ impl Proxy {
     pub fn new () -> Self {
         Proxy {}
     }
+
+    //not part of HubProxy because it would cause E0038
+    //this will now make the api usage awkward since the trait object now needs a downcast
+    //TODO abhi: rework on moving this to an appropriate place
+    fn on<T> (&self, event_name : String, f : fn(T)) {
+
+    }
 }
 
 impl HubProxy for Proxy { 
     fn invoke (&self, method : String) {
-
+        
     } 
 }
