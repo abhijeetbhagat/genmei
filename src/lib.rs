4e97080afb17bb739dc9b076e3a48034cb2b452c
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2017-11-26 17:17:55 +0530

    Add tests for Version

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,8 @@ mod hubresult;
 mod httpbasedtransport; 
 mod httpclient;
 mod subscription;
+mod urlbuilder;
+mod version;
 
 #[cfg(test)]
 mod tests {
@@ -28,6 +30,7 @@ mod tests {
     use hubproxy::Proxy;
     use std::mem;
     use futures::future::Future;
+    use version::Version;
 
     //http://localhost:8080/signalr/send?clientProtocol=1.4&transport=serverSentEvents&connectionData=[%7B%22Name%22:%22MyHub%22%7D]&connectionToken=AQAAANCMnd8BFdERjHoAwE%2FCl%2BsBAAAAJKIyAZXvi0e08Sl079QEAAAAAAACAAAAAAADZgAAwAAAABAAAABKuV%2Bxe15SC20qoS1GIkm0AAAAAASAAACgAAAAEAAAANuqwbda%2FDjBwm7ikQKzgCwoAAAAkgvwaH5thyZv8X9ug41XupjSvsRPTX9XV0Np2QnUA3xpEI6mtigCXRQAAADTkkV58tskB3sVw1IBT%2FoxWDt8IQ%3D%3D
     #[test]
@@ -58,5 +61,28 @@ mod tests {
         };
         assert_eq! (serde_json::to_string (&message).unwrap(), 
                     "{\"I\":\"9\",\"H\":\"MyHub\",\"M\":\"send\",\"A\":[]}");
+    }
+
+    #[test]
+    fn test_connection_headers_set() {
+        let mut connection = HubConnectionBuilder::new (String::from("http://localhost:8080"))
+            .use_default_url (false)
+            .finish();
+        connection.headers.insert(String::from("header"), String::from("value"));
+    }
+
+    #[test]
+    fn test_on_received_handler() {
+        let mut connection = HubConnectionBuilder::new (String::from("http://localhost:8080"))
+            .finish();
+        connection.on_received (Box::new (|s| { assert_eq! (s, String::from ("Hello from server"));}));
+    }
+
+    #[test]
+    fn test_versions() {
+        assert_eq!(Version::new(1,4), Version::new(1,4));
+        assert_ne!(Version::new(1,4), Version::new(1,3));
+        assert!(Version::new(1,4) < Version::new(1,5));
+        assert!(Version::new(1,4) < Version::new(2,5));
     }
 }
