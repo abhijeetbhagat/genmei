b64bee546ad587a241099e25e170355685f4dcac
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2017-12-09 01:27:39 +0530

    Add Connection as parameter to methods in ClientTransport

--- src/autotransport.rs
+++ src/autotransport.rs
@@ -2,6 +2,8 @@ use clienttransport::ClientTransport;
 use futures::future::Future;
 use negotiationresponse::NegotiationResponse;
 use httpclient::HttpClient;
+use urlbuilder::UrlBuilder;
+use connection::Connection;
 
 pub struct AutoTransport {
     http_client : Box<HttpClient>
@@ -16,21 +18,23 @@ impl AutoTransport {
 }
 
 impl ClientTransport for AutoTransport {
-    fn negotiate(&mut self) -> Box<Future<Item=NegotiationResponse, Error=()>> {
+    fn negotiate(&mut self, conn : &Connection, connection_data : String) -> Box<Future<Item=NegotiationResponse, Error=()>> {
+        unimplemented!();
+        let url = UrlBuilder::create_base_url(conn, "negotiate", None, connection_data.as_str());
+        let response = self.http_client.get(url.as_str());
+    }
+
+    fn start(&self, conn : &Connection) -> Box<Future<Item=(), Error=()>> {
+
         unimplemented!();
     }
 
-    fn start(&self, hub_name : String) -> Box<Future<Item=(), Error=()>> {
+    fn send(&self, conn : &Connection) -> Box<Future<Item=(), Error=()>> {
 
         unimplemented!();
     }
 
-    fn send(&self) -> Box<Future<Item=(), Error=()>> {
-
-        unimplemented!();
-    }
-
-    fn abort(&self) -> Box<Future<Item=(), Error=()>> {
+    fn abort(&self, conn : &Connection) -> Box<Future<Item=(), Error=()>> {
 
         unimplemented!();
     }
--- src/clienttransport.rs
+++ src/clienttransport.rs
@@ -1,11 +1,12 @@
 use futures::future::{FutureResult, Future};
 use negotiationresponse::NegotiationResponse;
+use connection::Connection;
 
 pub trait ClientTransport {
-    fn negotiate(&mut self) -> Box<Future<Item=NegotiationResponse, Error=()>>;
-    fn start(&self, hub_name : String) -> Box<Future<Item=(), Error=()>>;
-    fn send(&self) -> Box<Future<Item=(), Error=()>>;
-    fn abort(&self) -> Box<Future<Item=(), Error=()>>;
+    fn negotiate(&mut self, conn : &Connection, connection_data : String) -> Box<Future<Item=NegotiationResponse, Error=()>>;
+    fn start(&self, conn : &Connection) -> Box<Future<Item=(), Error=()>>;
+    fn send(&self, conn : &Connection) -> Box<Future<Item=(), Error=()>>;
+    fn abort(&self, conn : &Connection) -> Box<Future<Item=(), Error=()>>;
 }
 
 fn foo(ct : &ClientTransport) {}
