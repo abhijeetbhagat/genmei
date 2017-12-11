381e2c20f618cf6a321b8ecb8477b143e396d786
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2017-12-11 12:36:09 +0530

    Add skeletal implementation of ClientTransport for ServerSentEventsTransport

--- src/serversenteventstransport.rs
+++ src/serversenteventstransport.rs
@@ -1,2 +1,39 @@
+use clienttransport::ClientTransport;
+use futures::future::Future;
+use negotiationresponse::NegotiationResponse;
+use httpclient::HttpClient;
+use urlbuilder::UrlBuilder;
+use connection::Connection;
+
 pub struct ServerSentEventsTransport;
+
+impl ClientTransport for ServerSentEventsTransport {
+    fn negotiate(
+        &mut self,
+        url: &str,
+        connection_data: &str,
+        protocol: &str,
+    ) -> Box<Future<Item = NegotiationResponse, Error = ()>> {
+        unimplemented!();
+    }
+
+    fn start(
+        &self,
+        url: &str,
+        connection_data: &str,
+        connection_token: &str,
+        protocol: &str,
+    ) -> Box<Future<Item = (), Error = ()>> {
+        unimplemented!();
+    }
+
+
+    fn send(&self) -> Box<Future<Item = (), Error = ()>> {
+        unimplemented!();
+    }
+
+    fn abort(&self) -> Box<Future<Item = (), Error = ()>> {
+        unimplemented!();
+    }
+}
 
