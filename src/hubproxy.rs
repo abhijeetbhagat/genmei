d79a8fe3c54ceb75551897031c0e62f482636798
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2018-01-04 01:06:38 +0530

    Add proxy handlers that can take upto 7 args

--- src/hubproxy.rs
+++ src/hubproxy.rs
@@ -35,6 +35,13 @@ impl Proxy {
         }
     }
 
+    pub fn on(&mut self, event_name: String, f: Box<Fn()>) {
+        let mut subscription = self.subscribe(event_name);
+        subscription.set(Box::new(move |l| {
+            f();
+        }));
+    }
+
     //not part of HubProxy because it would cause E0038
     //this will now make the api usage awkward since the trait object now needs a downcast
     //TODO abhi: rework on moving this to an appropriate place
@@ -45,15 +52,171 @@ impl Proxy {
         let mut subscription = self.subscribe(event_name);
         subscription.set(Box::new(move |mut l| {
             println!("callback invoked with args {}", l[0]);
+            //TODO abhi: this is done to avoid shifting of elements from a Vec:remove()
             let v = std::mem::replace(&mut l[0], serde_json::json!(0));
             f(serde_json::from_value(v).unwrap());
         }));
     }
 
-    pub fn on(&mut self, event_name: String, f: Box<Fn()>) {
+    pub fn on_2_args<T1, T2>(&mut self, event_name: String, f: Box<Fn(T1, T2)>)
+    where
+        T1: 'static + serde::de::DeserializeOwned,
+        T2: 'static + serde::de::DeserializeOwned,
+    {
         let mut subscription = self.subscribe(event_name);
-        subscription.set(Box::new(move |l| {
-            f();
+        subscription.set(Box::new(move |mut l| {
+            println!("callback invoked with args {}", l[0]);
+            //TODO abhi: this is done to avoid shifting of elements from a Vec:remove()
+            let v1 = std::mem::replace(&mut l[0], serde_json::json!(0));
+            let v2 = std::mem::replace(&mut l[1], serde_json::json!(0));
+            f(
+                serde_json::from_value(v1).unwrap(),
+                serde_json::from_value(v2).unwrap(),
+            );
+        }));
+    }
+
+    pub fn on_3_args<T1, T2, T3>(&mut self, event_name: String, f: Box<Fn(T1, T2, T3)>)
+    where
+        T1: 'static + serde::de::DeserializeOwned,
+        T2: 'static + serde::de::DeserializeOwned,
+        T3: 'static + serde::de::DeserializeOwned,
+    {
+        let mut subscription = self.subscribe(event_name);
+        subscription.set(Box::new(move |mut l| {
+            println!("callback invoked with args {}", l[0]);
+            //TODO abhi: this is done to avoid shifting of elements from a Vec:remove()
+            let v1 = std::mem::replace(&mut l[0], serde_json::json!(0));
+            let v2 = std::mem::replace(&mut l[1], serde_json::json!(0));
+            let v3 = std::mem::replace(&mut l[2], serde_json::json!(0));
+            f(
+                serde_json::from_value(v1).unwrap(),
+                serde_json::from_value(v2).unwrap(),
+                serde_json::from_value(v3).unwrap(),
+            );
+        }));
+    }
+
+    pub fn on_4_args<T1, T2, T3, T4>(&mut self, event_name: String, f: Box<Fn(T1, T2, T3, T4)>)
+    where
+        T1: 'static + serde::de::DeserializeOwned,
+        T2: 'static + serde::de::DeserializeOwned,
+        T3: 'static + serde::de::DeserializeOwned,
+        T4: 'static + serde::de::DeserializeOwned,
+    {
+        let mut subscription = self.subscribe(event_name);
+        subscription.set(Box::new(move |mut l| {
+            println!("callback invoked with args {}", l[0]);
+            //TODO abhi: this is done to avoid shifting of elements from a Vec:remove()
+            let v1 = std::mem::replace(&mut l[0], serde_json::json!(0));
+            let v2 = std::mem::replace(&mut l[1], serde_json::json!(0));
+            let v3 = std::mem::replace(&mut l[2], serde_json::json!(0));
+            let v4 = std::mem::replace(&mut l[3], serde_json::json!(0));
+            f(
+                serde_json::from_value(v1).unwrap(),
+                serde_json::from_value(v2).unwrap(),
+                serde_json::from_value(v3).unwrap(),
+                serde_json::from_value(v4).unwrap(),
+            );
+        }));
+    }
+
+    pub fn on_5_args<T1, T2, T3, T4, T5>(
+        &mut self,
+        event_name: String,
+        f: Box<Fn(T1, T2, T3, T4, T5)>,
+    ) where
+        T1: 'static + serde::de::DeserializeOwned,
+        T2: 'static + serde::de::DeserializeOwned,
+        T3: 'static + serde::de::DeserializeOwned,
+        T4: 'static + serde::de::DeserializeOwned,
+        T5: 'static + serde::de::DeserializeOwned,
+    {
+        let mut subscription = self.subscribe(event_name);
+        subscription.set(Box::new(move |mut l| {
+            println!("callback invoked with args {}", l[0]);
+            //TODO abhi: this is done to avoid shifting of elements from a Vec:remove()
+            let v1 = std::mem::replace(&mut l[0], serde_json::json!(0));
+            let v2 = std::mem::replace(&mut l[1], serde_json::json!(0));
+            let v3 = std::mem::replace(&mut l[2], serde_json::json!(0));
+            let v4 = std::mem::replace(&mut l[3], serde_json::json!(0));
+            let v5 = std::mem::replace(&mut l[4], serde_json::json!(0));
+            f(
+                serde_json::from_value(v1).unwrap(),
+                serde_json::from_value(v2).unwrap(),
+                serde_json::from_value(v3).unwrap(),
+                serde_json::from_value(v4).unwrap(),
+                serde_json::from_value(v5).unwrap(),
+            );
+        }));
+    }
+
+    pub fn on_6_args<T1, T2, T3, T4, T5, T6>(
+        &mut self,
+        event_name: String,
+        f: Box<Fn(T1, T2, T3, T4, T5, T6)>,
+    ) where
+        T1: 'static + serde::de::DeserializeOwned,
+        T2: 'static + serde::de::DeserializeOwned,
+        T3: 'static + serde::de::DeserializeOwned,
+        T4: 'static + serde::de::DeserializeOwned,
+        T5: 'static + serde::de::DeserializeOwned,
+        T6: 'static + serde::de::DeserializeOwned,
+    {
+        let mut subscription = self.subscribe(event_name);
+        subscription.set(Box::new(move |mut l| {
+            println!("callback invoked with args {}", l[0]);
+            //TODO abhi: this is done to avoid shifting of elements from a Vec:remove()
+            let v1 = std::mem::replace(&mut l[0], serde_json::json!(0));
+            let v2 = std::mem::replace(&mut l[1], serde_json::json!(0));
+            let v3 = std::mem::replace(&mut l[2], serde_json::json!(0));
+            let v4 = std::mem::replace(&mut l[3], serde_json::json!(0));
+            let v5 = std::mem::replace(&mut l[4], serde_json::json!(0));
+            let v6 = std::mem::replace(&mut l[5], serde_json::json!(0));
+            f(
+                serde_json::from_value(v1).unwrap(),
+                serde_json::from_value(v2).unwrap(),
+                serde_json::from_value(v3).unwrap(),
+                serde_json::from_value(v4).unwrap(),
+                serde_json::from_value(v5).unwrap(),
+                serde_json::from_value(v6).unwrap(),
+            );
+        }));
+    }
+
+    pub fn on_7_args<T1, T2, T3, T4, T5, T6, T7>(
+        &mut self,
+        event_name: String,
+        f: Box<Fn(T1, T2, T3, T4, T5, T6, T7)>,
+    ) where
+        T1: 'static + serde::de::DeserializeOwned,
+        T2: 'static + serde::de::DeserializeOwned,
+        T3: 'static + serde::de::DeserializeOwned,
+        T4: 'static + serde::de::DeserializeOwned,
+        T5: 'static + serde::de::DeserializeOwned,
+        T6: 'static + serde::de::DeserializeOwned,
+        T7: 'static + serde::de::DeserializeOwned,
+    {
+        let mut subscription = self.subscribe(event_name);
+        subscription.set(Box::new(move |mut l| {
+            println!("callback invoked with args {}", l[0]);
+            //TODO abhi: this is done to avoid shifting of elements from a Vec:remove()
+            let v1 = std::mem::replace(&mut l[0], serde_json::json!(0));
+            let v2 = std::mem::replace(&mut l[1], serde_json::json!(0));
+            let v3 = std::mem::replace(&mut l[2], serde_json::json!(0));
+            let v4 = std::mem::replace(&mut l[3], serde_json::json!(0));
+            let v5 = std::mem::replace(&mut l[4], serde_json::json!(0));
+            let v6 = std::mem::replace(&mut l[5], serde_json::json!(0));
+            let v7 = std::mem::replace(&mut l[6], serde_json::json!(0));
+            f(
+                serde_json::from_value(v1).unwrap(),
+                serde_json::from_value(v2).unwrap(),
+                serde_json::from_value(v3).unwrap(),
+                serde_json::from_value(v4).unwrap(),
+                serde_json::from_value(v5).unwrap(),
+                serde_json::from_value(v6).unwrap(),
+                serde_json::from_value(v7).unwrap(),
+            );
         }));
     }
 
